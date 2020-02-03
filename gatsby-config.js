6d79d22ee58f3228f1864145dd702f10e5f9eca3
Rowan Richardson <rr.designweb@gmail.com>
2020-02-03 16:57:56 +1100

    Changed blog title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Cleaning Gem Melbourne`,
+    title: `Test Blog`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
