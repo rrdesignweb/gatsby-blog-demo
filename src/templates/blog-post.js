e9fa01b23a4049957f8d6e14c99520f299e2a150
Rowan Richardson <rr.designweb@gmail.com>
2020-02-05 16:31:43 +1100

    Added rich text component to render media/images & other html elements

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,25 +1,43 @@
 import React from "react"
-
 import { graphql } from "gatsby"
+import { BLOCKS } from "@contentful/rich-text-types"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 import Layout from "../components/layout"
-
-const SinglePost = ({ data }) => {
-  const post = data.contentfulBlogPost
-  return (
-    <Layout>
-      <h1>{post.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: post.body }} />
-    </Layout>
-  )
-}
+import SEO from "../components/seo"
 
 export const query = graphql`
   query($slug: String!) {
     contentfulBlogPost(slug: { eq: $slug }) {
       title
       publishedDate(formatString: "MMMM Do, YYYY")
+      body {
+        json
+      }
     }
   }
 `
+
+const SinglePost = ({ data }) => {
+  const post = data.contentfulBlogPost
+  const options = {
+    renderNode: {
+      [BLOCKS.EMBEDDED_ASSET]: node => {
+        const { title, file } = node.data.target.fields
+        const alt = title["en-US"]
+        const url = file["en-US"].url
+        return <img alt={alt} src={url} />
+      },
+    },
+  }
+  return (
+    <Layout>
+      <SEO title={post.title} />
+      <h1>{post.title}</h1>
+      <p>{post.publishedDate}</p>
+      {documentToReactComponents(post.body.json, options)}
+    </Layout>
+  )
+}
+
 export default SinglePost
